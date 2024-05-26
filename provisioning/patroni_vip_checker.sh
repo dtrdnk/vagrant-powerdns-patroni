d89c0de731fd50404ad0f3b977e3c7ef2430c7df
dtrdnk <>
2024-05-26 09:42:18 +0300

    Small refactoring patroni_vip_checker.sh

--- provisioning/patroni_vip_checker.sh
+++ provisioning/patroni_vip_checker.sh
@@ -5,16 +5,8 @@
 
 patroni_node_state=$(curl -s http://127.0.0.1:8008/leader | jq -r '.role')
 
-function compare_vip_state_with_patroni_role() {
-    if [[ "${patroni_node_state}" == "master" ]]; then
-      exit 0
-    else
-      exit 1
-    fi
-}
-
-function main() {
-    compare_vip_state_with_patroni_role
-}
-
-main
+if [[ "${patroni_node_state}" == "master" ]]; then
+  exit 0
+else
+  exit 1
+fi
